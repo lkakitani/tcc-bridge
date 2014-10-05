6ed4adced96d080ae48e98f075eee0f5eb5d3541
Leonardo Pedro Kakitani <leonardo.kakitani@gmail.com>
2014-10-04 21:35:12 -0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 tcc-bridge
 ==========
+
+Trabalho de conclusão de curso para o projeto ANJELS - módulo bridge.
--- xml_parser/bridge_servlet/writer.cpp
+++ xml_parser/bridge_servlet/writer.cpp
@@ -8,6 +8,9 @@ int main() {
 	std::string bid = "b0003";
 	std::string sid = "002";
 	std::string recipient = "1";
+	std::string request = "sensor"; // sensor | function | program
+	std::string lightSensor = "1231";
+	std::string presenceSensor = "1";
 
 	xmlFile.open("bridge_response.xml");
 
@@ -17,10 +20,28 @@ int main() {
 	xmlFile << "<bridgemsg bid=\"" << bid << "\">\n";
 	xmlFile << "\t<sid>" << sid << "</sid>\n";
 	xmlFile << "\t<recipient>" << recipient << "</recipient>\n";
+	xmlFile << "\t<request_type>" << request << "</request_type>\n";
+
 	xmlFile << "\t<response>\n";
+	if (request == "sensor") {
+
+		// light sensor
+		xmlFile << "\t\t<sensor type=\"LUM\">\n";
+		xmlFile << "\t\t\t<value>" << lightSensor << "</value>\n";
+		xmlFile << "\t\t</sensor>\n";
+		// presence sensor
+		xmlFile << "\t\t<sensor type=\"PRES\">\n";
+		xmlFile << "\t\t\t<value>" << presenceSensor << "</value>\n";
+		xmlFile << "\t\t</sensor>\n";
+
+	} else if (request == "function" || request == "program") {
+		xmlFile << "\t\t<return>ok</return>\n";
+	}
 
 	xmlFile << "\t</response>\n";
 	xmlFile << "</bridgemsg>";
 	xmlFile.close();
+
+	std::cout << "Write successful." << std::endl;
 	return 0;
 }
